ee8a1cc87c2b9861fd74153beaab37802ff88a4d
Jon Higgins <jon@jonhiggins.co.uk>
2014-11-19 10:04:59 +0000

    chart.js - switch to 1.0.1 beta for responsive version

--- app/scripts/directives/chart.js
+++ app/scripts/directives/chart.js
@@ -1,49 +1,41 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc directive
- * @name giveyTeamFundraisingApp.directive:chart
- * @description
- * # chart
- */
+  /**
+   * @ngdoc directive
+   * @name giveyTeamFundraisingApp.directive:chart
+   * @description
+   * # chart
+   */
 
-function chartDirective() {
-  return {
-      template: '<div>!!!</div>',
-      restrict: 'A',
-      scope: {
-        data: '=chartData',
-        options: '=chartOptions',
-        type: '@chartType',
-        legend: '=chartLegend',
-        chart: '=chart'
-      },
-      link: function postLink($scope, $elem) {
+  function chartDirective() {
+    return {
+        restrict: 'A',
+        scope: {
+          data: '=chartData',
+          type: '@chartType'
+        },
+        link: function postLink($scope, $elem) {
 
-        var ctx = $elem[0].getContext( '2d' ),
+          var ctx = $elem[0].getContext( '2d' ),
             chart = new Chart( ctx ),
-            chartObj,
-            parentWidth = $elem.parent().prop('offsetWidth');
+            chartObj;
 
-        $elem.css({
-          'width': parentWidth,
-          'height': 'auto'
-        });
-
-        $scope.$watch(
-          'data',
-          function ( value ) {
-            if ( chartObj ) {
-              chartObj.destroy();
+          $scope.$watch(
+            'data',
+            function ( value ) {
+              if ( chartObj ) {
+                chartObj.destroy();
+              }
+              if ( value ) {
+                chartObj = chart[ $scope.type ]( $scope.data, { responsive : true} );
+              }
             }
-            if ( value ) {
-              chartObj = chart[ $scope.type ]( $scope.data, $scope.options );
-            }
-          }
-        );
-      }
-    };
-}
+          );
+        }
+      };
+  }
 
-angular.module('giveyTeamFundraisingApp')
-  .directive('chart', chartDirective);
+  angular.module('giveyTeamFundraisingApp')
+    .directive('chart', chartDirective);
+})();
