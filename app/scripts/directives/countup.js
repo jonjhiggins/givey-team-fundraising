7ece31f7d9881344e38031b2f79ea7ee1065d5a3
Jon Higgins <jon@jonhiggins.co.uk>
2014-11-20 14:02:32 +0000

    countups only occuring once

--- app/scripts/directives/countup.js
+++ app/scripts/directives/countup.js
@@ -1,33 +1,59 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc directive
- * @name giveyTeamFundraisingApp.directive:countUp
- * @description
- * # countUp
- */
-angular.module('giveyTeamFundraisingApp')
-  .directive('countUp', function () {
+  /**
+   * @ngdoc directive
+   * @name giveyTeamFundraisingApp.directive:countUp
+   * @description
+   * # countUp
+   */
+
+  var countUpDirective = function() {
+
+    var postLink = function($scope, $elem) {
+
+      var totalAvailable,
+          inViewStatus,
+          id = 'count-up' + Math.floor((Math.random()*9999));
+
+      $elem.prop('id', id);
+
+      $scope.$watch(
+        'total', function ( value ) {
+          totalAvailable = value;
+          renderCountUp();
+        }
+      );
+
+      $scope.$watch(
+        'inViewStatus', function( status ) {
+          inViewStatus = status;
+          renderCountUp();
+        }
+      );
+
+      var renderCountUp = function() {
+        if ( totalAvailable && inViewStatus ) {
+          var numAnim = new countUp(id, 0, (totalAvailable / 100), 0, 1.5);
+          $elem.addClass('count-up--currency');
+          numAnim.start();
+          totalAvailable = false;
+        }
+      };
+    };
+
     return {
       restrict: 'A',
       scope: {
-        total: '=total'
+        total: '=total',
+        inViewStatus: '=inViewStatus'
       },
-      link: function postLink($scope, $elem) {
-
-        var id = 'count-up' + Math.floor((Math.random()*9999));
-        $elem.prop('id', id);
-
-        $scope.$watch(
-          'total',
-          function ( value ) {
-            if ( value ) {
-              var numAnim = new countUp(id, 0, (value / 100), 0, 1.5);
-              $elem.addClass('count-up--currency');
-              numAnim.start();
-            }
-          }
-        );
-      }
+      link: postLink
     };
-  });
+  };
+
+
+
+  angular.module('giveyTeamFundraisingApp')
+    .directive('countUp', countUpDirective);
+})();
