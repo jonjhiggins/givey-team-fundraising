fe8a0b012c9203d4d00302411c174d2ead97c8e7
Jon Higgins <jon@jonhiggins.co.uk>
2014-11-20 09:57:24 +0000

    xit missing tests :S

--- test/spec/directives/countup.js
+++ test/spec/directives/countup.js
@@ -12,7 +12,7 @@ describe('Directive: countUp', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
+  xit('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<count-up></count-up>');
     element = $compile(element)(scope);
     expect(element.text()).toBe('this is the countUp directive');
